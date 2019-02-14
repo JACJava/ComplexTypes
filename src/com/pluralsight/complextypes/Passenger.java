ea4e5797704eb9f7166a0f99e82e0c9e75a484a5
JACJava <46228469+JACJava@users.noreply.github.com>
2019-02-14 12:47:04 -0600

    Add files via upload

--- src/com/pluralsight/complextypes/Passenger.java
+++ src/com/pluralsight/complextypes/Passenger.java
@@ -1,20 +1,43 @@
 package com.pluralsight.complextypes;
 
-public class Passenger {
+public final class Passenger { // final means you can't inherit from it
 
     private int checkedBags;
     private int freeBags;
 
     private double perBagFee;
 
-    public int getcheckedBags() {return checkedBags;}
+    // constructor that doesn't take arguments to allow it to be called with no parameters
+    public Passenger() {}
+
+    // constructor that accepts number of free bags
+    public Passenger (int freeBags) {
+        // make call down to baggage fee constructor
+        this(freeBags > 1 ? 25.0d : 50.0d);
+        this.freeBags = freeBags;
+    }
+
+    // constructor that accepts number of free bags and checked bags
+    public Passenger (int freeBags, int checkedBags) {
+        this(freeBags); // will call into the constructor with no parameters
+        //this.freeBags = freeBags; does the same thing as prior line
+        this.checkedBags = checkedBags;
+    }
+
+    // constructor that allows us to identify bag fee, make this private
+    private Passenger (double perBagFee) {
+        this.perBagFee = perBagFee;
+    }
+
+
+    public int getCheckedBags() {return checkedBags;}
     public void setCheckedBags(int checkedBags) {this.checkedBags = checkedBags;}
 
     public int getFreeBags() {return freeBags;}
     public void setfreeBags(int freeBags) {this.freeBags = freeBags;}
 
     // Since perbagfee is created by this class, shouldn't be updated from outside the class
-    public double getperBagFee() {return perBagFee;}
+    public double getPerBagFee() {return perBagFee;}
 
     public void figureOutBagFee(){
         perBagFee = checkedBags * 100;
